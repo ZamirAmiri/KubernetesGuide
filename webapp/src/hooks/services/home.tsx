83db8d637afef27af0b53ff91ff9a9a0c78eb6bd
Zamir <ZamirAmiri@github.com>
2022-03-15 21:53:38 +0100

    Temp push

--- webapp/src/hooks/services/home.tsx
+++ webapp/src/hooks/services/home.tsx
@@ -1,5 +1,6 @@
 export async function getHome(){
-    const url:string = 'http://dev.kubeguide.com/rest/api/v1';
+    const URL:string|undefined = process.env.REACT_APP_BASE_URL;
+    const url:string = URL?URL:'http://dev.kubeguide.com/rest/api/v1';
     const response:Response = await fetch(url);
     let json = await response.json();
     return json;
